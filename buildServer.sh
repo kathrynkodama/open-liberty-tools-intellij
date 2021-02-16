bd75b46258678491eb62f6c72d5299864d994c52
Ryan Zegray <ryan.zegray@ibm.com>
2021-02-16 11:36:26 -0500

    Add buildscript

---
+++ buildServer.sh
@@ -0,0 +1,2 @@
+cd ../lemminx && mvn clean verify && cp org.eclipse.lemminx/target/org.eclipse.lemminx-uber.jar ../open-liberty-tools-intellij/src/main/resources/
+# TODO: Build liberty-lemminx extension and copy it over 
--- src/main/java/io/openliberty/tools/intellij/ls/LibertyPreloadingActivity.java
+++ src/main/java/io/openliberty/tools/intellij/ls/LibertyPreloadingActivity.java
@@ -10,8 +10,16 @@ public class LibertyPreloadingActivity extends PreloadingActivity {
 
     @Override
     public void preload(ProgressIndicator indicator) {
+
         String[] command = new String[] { "java", "-jar",
                 "/Users/rzegray/Developer/github.com/openliberty/open-liberty-tools-intellij/src/main/resources/org.eclipse.lemminx-uber.jar" };
+        // String[] command = new String[] { "java", "-cp",
+        // "/Users/rzegray/Developer/github.com/openliberty/open-liberty-tools-intellij/src/main/resources/org.eclipse.lemminx-uber.jar:/Users/rzegray/Developer/github.com/openliberty/open-liberty-tools-intellij/src/main/resources/lemminx-liberty-1.0-SNAPSHOT-jar-with-dependencies.jar
+        // org.eclipse.lemminx.XMLServerLauncher" };
+
+        // String[] command2 = new String[] { "java", "-cp",
+        // "/Users/rzegray/Developer/github.com/openliberty/open-liberty-tools-intellij/src/main/resources/org.eclipse.lemminx-uber.jar
+        // org.eclipse.lemminx.XMLServerLauncher" };
         IntellijLanguageClient.addServerDefinition(new RawCommandServerDefinition("xml", command));
     }
 
